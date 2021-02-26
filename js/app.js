51913a7f751d87f6fa2faac4d9f4f01c109cc246
yeison mejia <alexmejia992@gmail.com>
2021-02-26 10:11:48 -0500

    adding images

--- js/app.js
+++ js/app.js
@@ -1,3 +1,36 @@
-const tl = gsap.timeline({defaults: {easy: 'power1.out'} });
+/* import { gsap } from "gsap/dist/gsap";
+console.log(gsap)
+ */
+/* const tl = gsap.timeline({defaults: {easy: 'power1.out'} });
 
-tl.fromTo('.lexweb', {opacity: 0}, {opacity: 1, duration: 4});
+tl.fromTo('.lexweb', {opacity: 0}, {opacity: 1, duration: 4}); */
+
+
+/* function iniciarApp(){
+
+}
+ */
+
+
+function tecnologias() {
+
+    const galeria = document.querySelector('#tecnologias__lista');
+
+    for(let i = 1; i <= 10; i++){
+        const imagen = document.createElement('IMG');
+        imagen.src = `/img/${i}.svg`;
+        imagen.classList.add('skils__imagen')
+        imagen.dataset.imagenId = i;
+
+        /* const lista = document.createElement('LI');
+        lista.appendChild(imagen); */
+
+        galeria.appendChild(imagen)
+    }
+}
+
+tecnologias();
+
+/* imagenes.forEach(imagen => {
+    
+}); */
