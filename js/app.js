ca98a2266e7d213a85d810a18010f55e1e1b594c
yeison mejia <alexmejia992@gmail.com>
2021-03-01 08:36:05 -0500

    add styles and images

--- js/app.js
+++ js/app.js
@@ -1,36 +1,41 @@
-/* import { gsap } from "gsap/dist/gsap";
-console.log(gsap)
- */
-/* const tl = gsap.timeline({defaults: {easy: 'power1.out'} });
-
-tl.fromTo('.lexweb', {opacity: 0}, {opacity: 1, duration: 4}); */
-
-
-/* function iniciarApp(){
-
-}
- */
-
-
 function tecnologias() {
 
     const galeria = document.querySelector('#tecnologias__lista');
 
-    for(let i = 1; i <= 10; i++){
+    for(let i = 1; i <= 12; i++){
         const imagen = document.createElement('IMG');
         imagen.src = `/img/${i}.svg`;
-        imagen.classList.add('skils__imagen')
+        imagen.classList.add(`skils__imagen`)
+        imagen.classList.add(`skils__imagen${i}`)
         imagen.dataset.imagenId = i;
+
+        const contenedorDiv = document.createElement('DIV')
+        contenedorDiv.classList.add('tecnologias__contenedor')
+
 
         /* const lista = document.createElement('LI');
         lista.appendChild(imagen); */
+        contenedorDiv.appendChild(imagen)
+        galeria.appendChild(contenedorDiv)
 
-        galeria.appendChild(imagen)
     }
+        
 }
 
 tecnologias();
 
-/* imagenes.forEach(imagen => {
+
+        // GSAP custom code goes here
+gsap.registerPlugin(ScrollTrigger);   
+const tl = gsap.timeline();
+
+gsap.to('.skils__imagen', {scrollTrigger: '.skils__imagen', rotationX:360, duration: 3,})
+/* gsap.from('.skils__imagen', {scrollTrigger: '.skils__imagen', duration: 1, opacity: 1,})
+ */
+
+
     
-}); */
+
+
+
+
